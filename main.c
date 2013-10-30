07bbbabbaa0708bac873a4cd2af910e3b6b0b8d6
LeviHarman <serpentinexcubed@gmail.com>
2013-10-30 11:21:06 -0500

    added a drawing function

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 
 #include <stdio.h>
 #include <allegro5/allegro.h>
-
 #include "link1.h"
 #include "link2.h"
 
 const int SCREEN_W = 400;
 const int SCREEN_H = 400;
+
+void draw_bg_green(ALLEGRO_DISPLAY **display)
+{
+  al_set_target_backbuffer(*display);
+  al_clear_to_color(al_map_rgb(0,100,0));
+  al_flip_display();
+}
 
 int main(void)
 {
@@ -16,10 +22,10 @@ int main(void)
   al_init();
   
   ALLEGRO_DISPLAY *display = al_create_display(SCREEN_W, SCREEN_H);
-  
 
-  printf("link1 returns %d", a);
-  printf("link2 returns %d", b);
+  draw_bg_green(&display);
+
+  al_rest(5.0);
 
   al_destroy_display(display);
   
