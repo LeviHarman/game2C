586f33f19abca1a59fe090a7f482ada3d4dea3b5
Levi Harman <serpentinexcubed@gmail.com>
2013-10-30 23:45:05 -0500

    added draw main where I will put conditions on which things to draw in a loop in the future

--- draw.c
+++ draw.c
@@ -1,21 +1,30 @@
+
 /*
- * I will use this file to store drawing functions that do certain things
- * 
+ * I will use this file to store drawing functions.
  */
 #include<allegro5/allegro.h>
 #include<allegro5/allegro_primitives.h>
 
-void draw_bg_green(ALLEGRO_DISPLAY **display)
+void draw_bg_green()
 {
-  al_set_target_backbuffer(*display);
   al_clear_to_color(al_map_rgb(0,50,0));
-  al_flip_display();
 }
 
-void draw_square(ALLEGRO_DISPLAY **display)
+void draw_square()
+{
+  al_draw_filled_rectangle(2.5, 30.5, 55, 55, al_map_rgb(50,0,0));
+}
+
+void draw_main(ALLEGRO_DISPLAY **display)
 {
   al_set_target_backbuffer(*display);
-  al_draw_filled_rectangle(2.5, 30.5, 55, 55, al_map_rgb(50,0,0));
+  
+  draw_bg_green();
   al_flip_display();
+  al_rest(2.0);
+  
+  draw_square();
+  al_flip_display();
+  al_rest(2.0);
 }
 
