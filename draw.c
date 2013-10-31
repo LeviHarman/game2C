54af559f27086d651de8fdbff2be2df85a4cdb69
LeviHarman <serpentinexcubed@gmail.com>
2013-10-30 21:53:19 -0500

    removed extra parameter

--- draw.c
+++ draw.c
@@ -1,10 +1,21 @@
-
+/*
+ * I will use this file to store drawing functions that do certain things
+ * 
+ */
 #include<allegro5/allegro.h>
+#include<allegro5/allegro_primitives.h>
 
 void draw_bg_green(ALLEGRO_DISPLAY **display)
 {
   al_set_target_backbuffer(*display);
-  al_clear_to_color(al_map_rgb(0,100,0));
+  al_clear_to_color(al_map_rgb(0,50,0));
+  al_flip_display();
+}
+
+void draw_square(ALLEGRO_DISPLAY **display)
+{
+  al_set_target_backbuffer(*display);
+  al_draw_filled_rectangle(2.5, 3.5, 5.5, 5.5, al_map_rgb(50,0,0));
   al_flip_display();
 }
 
