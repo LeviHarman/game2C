88f0158d276a185045f496ca51bc2d4894a9d7ad
lhm <lhm@lhm>
2013-11-10 15:30:26 -0600

    added algorithms for calculating top triangle for sierpinski gasket

--- draw.c
+++ draw.c
@@ -1,7 +1,8 @@
 
 /*
- * I will use this file to store drawing functions.
+ * This file is used to store drawing functions.
  */
+
 #include<stdio.h>
 #include<allegro5/allegro.h>
 #include<allegro5/allegro_primitives.h>
@@ -19,63 +20,48 @@ void draw_square()
 void draw_triangle()
 {
 
-  int x1 = 200;
-  int y1 = 0;
-  int x2 = 0;
-  int y2 = 400;
-  int x3 = 400;
-  int y3 = 400;
+  /*
+   * initial triangle
+   *  try to fill screen
+   */
 
-  al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(0,50,50));
+  int p1[2]; //initial triangle point 1
+  int p2[2]; //initial triangle point 2
+  int p3[2]; //initial triangle point 3
+
+  p1[0] = 200;
+  p1[1] = 0;
+  p2[0] = 0;
+  p2[1] = 400;
+  p3[0] = 400;
+  p3[1] = 400;
+
+  al_draw_filled_triangle(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1], al_map_rgb(0,50,50));
   al_flip_display();
   al_rest(1.0);
 
   /*
    * top triangle
-   *  keep x1, and y1 the same
-   *  add some to x2 and remove some from y2 so that they are twice as close
-   *  subtract 1/4 from x3, and 1/2 from y3
-   *  y2 and y3 always the same
    */
-  //al_draw_filled_triangle(200, 0, 100, 200, 300, 200, al_map_rgb(50,50,50));
-  x1 = 200; //doesnt change
-  y1 = 0; //doesn't change
-  x2 = 100; //x2 and x3 always add up to 400 x2 gets larger as the triangle gets smaller
-  y2 = 200; //always same as y3
-  x3 = 300; //x2 and x3 always add up to 400 x3 gets smaller as the triangle gets smaller
-  y3 = 200; //always same as y2
 
-  al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(50,50,50));
+  int tp1[2]; //top triangle point 1 (top)
+  int tp2[2]; //top triangle point 2 (bottom left)
+  int tp3[2]; //top triangle point 3 (bottom right)
   
-  x1 = 200;
-  y1 = 0;
-  x2 = 250;
-  y2 = 100;
-  x3 = 150;
-  y3 = 100;
+  //Algorithm for calculating top triangles first point (top)
+  tp1[0] = x1;  //doesnt change
+  tp1[1] = x2;  //doesn't change
 
-  al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(100,50,50));
-  
-  
-  printf("y2/3: %d\n", y2/3);
-  printf("y2-(y2/3): %d\n", y2-(y2/3));
+  //Algorithm for calculating top triangles second point (bottom left)
+  tp2[1] = y2 / 2;      //always same as y3
+  tp2[0] = tp2[1] / 2;  //x2 and x3 always add up to 400 x2 gets larger as the triangle gets smaller
 
-  //al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(50,50,50));
-  //al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(50,50,50));
-  
-  //these are to mark corners
-  al_draw_filled_rectangle(x1, y1, x1+20, y1+20, al_map_rgb(70,10,65));//top point
-  al_draw_filled_rectangle(x2, y2, x2+20, y2-20, al_map_rgb(70,140,65));//bottom left point
-  al_draw_filled_rectangle(x3, y3, x3-20, y3-20, al_map_rgb(0,10,65));//bottom right point
+  //Algorithm for calculating top triangles third point (bottom right)
+  tp3[0] = 400 - tx2;   //x2 and x3 always add up to 400 x3 gets smaller as the triangle gets smaller
+  tp3[1] = ty2;         //always same as y2
 
-  int i;
-  for (i = 0; i < 3; i++)
-    {
-
-    }
+  al_draw_filled_triangle(tp1[0], tp1[1], tp2[0], tp2[1], tp3[0], tp3[1], al_map_rgb(50,50,50));
 }
-
-
 
 void draw_main(ALLEGRO_DISPLAY **display)
 {
